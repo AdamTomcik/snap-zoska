3b2d0acd4dd261cf8c9ce71d61c642ad23b231b3
adamtomcik <adamtomcik4@gmail.com>
2024-09-27 12:35:20 +0200

    Basic page structure complete

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,8 @@
 // src/app/(home)/page.tsx
 
 import Typography from '@mui/material/Typography';
-export const metadata = {title:"domov | ZoskaSnap "};
 
+export const metadata = {title:"domov | ZoskaSnap "};
 
 export default function Home() {
   return (
---
+++ src/app/prispevok/[id]/page.tsx
@@ -0,0 +1,11 @@
+// src/app/prispevok/[id]/page.tsx
+
+import Typography from '@mui/material/Typography';
+
+export const metadata = {title:"Detailny zoznam prispevkov ZoskaSnap "};
+
+export default function PostDetail() {
+  return (
+    <Typography>Detailny zoznam prispevkov</Typography>
+  );
+}
